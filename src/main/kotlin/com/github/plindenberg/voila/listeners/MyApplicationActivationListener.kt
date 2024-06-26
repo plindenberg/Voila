c1b65c91c207f72940e4f302cc33a2667d6f5b1a
GitHub Action <action@github.com>
2024-06-26 07:56:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/plindenberg/voila/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/plindenberg/voila/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.plindenberg.voila.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/plindenberg/voila/services/MyProjectService.kt
+++ src/main/kotlin/com/github/plindenberg/voila/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.plindenberg.voila.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.plindenberg.voila.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
